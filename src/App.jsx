c08b95d4a645ea60044789ed8ac9f06b408492e8
ryanr416 <ryanrichardson0993@gmail.com>
2023-08-01 09:42:35 -0400

    added link to sign up page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import SignupPage from "./pages/SignupPage/SignupPage"
+import LoginPage from "./pages/LoginPage/LoginPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react"
+import {
+	Button,
+	Form,
+	Grid,
+	Header,
+	Image,
+	Message,
+	Segment,
+  } from "semantic-ui-react";
+
+
+export default function SignUpPage() {
+
+    const [state, setState] = useState({
+            username: '',
+            email: '',
+            password: '',
+            passwordConf: '',
+            bio: ''
+    })
+
+    const [error, setError] = useState(''); 
+
+    // handlechange is being used to detect what key is being typed
+    function handleChange(e) {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+
+ async function handleSubmit(e){
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append('photo', selectedFile);
+    formData.append('username', state.username);
+    formData.append('password', state.password);
+    formData.append('email', state.email);
+    formData.append('bio', state.bio);
+    }
+
+return (
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+       <Header as="h2" color="purple" textAlign="center">
+        <Image src="" /> Sign Up
+      </Header>
+      <Form autoComplete="off" onSubmit={handleSubmit}>
+        <Segment stacked>
+          <Form.Input
+            name="username"
+            placeholder="username"
+            value={state.username}
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={state.email}
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+            name="password"
+            type="password"
+            placeholder="password"
+            value={state.password}
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+            name="passwordConf"
+            type="password"
+            placeholder="Confirm Password"
+            value={state.passwordConf}
+            onChange={handleChange}
+            required
+          />
+          <Form.TextArea
+            label="bio"
+            name="bio"
+            placeholder="Tell us about yourself"
+            value={state.bio}
+            onChange={handleChange}
+          />
+          <Form.Field>
+            <Form.Input
+              type="file"
+              name="photo"
+              placeholder="upload profile pic"
+              
+            />
+          </Form.Field>
+          <Button type="submit" className="btn">
+            Signup
+          </Button>
+        </Segment>
+        {error ? <ErrorMessage error={error} /> : null}
+      </Form>
+    </Grid.Column>
+  </Grid>
+
+    );
+
+}
+
+
