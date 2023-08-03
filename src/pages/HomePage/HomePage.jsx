d34b7f56d49e1d3aba0dbd63ffecd64ed06650d6
ryanr416 <ryanrichardson0993@gmail.com>
2023-08-03 09:37:56 -0400

    added cards for weather details

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
 
+import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import "./HomePage.css";
+
 export default function SearchWeatherForm({getWeather}){
 
+
+
+    
     const [weatherFormState, setWeatherFormState] = useState('')
 
     function handleChange(e) {
@@ -17,9 +24,46 @@ export default function SearchWeatherForm({getWeather}){
 
     return (
         <form onSubmit={handleSubmit}>
+                <Card>
+      <Card.Img src="https://picsum.photos/200/300" />
+      <Card.Body>
+        <Card.Title>Title Text</Card.Title>
+        <Card.Text>
+          Here's some fillllllller text
+        </Card.Text>
+        <div className="d-flex justify-content-between">
+          <Button className="ms-3">Add</Button>
+          <Button className="me-3">Remove</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  
             <input type="text" placeholder="Search Your Local City For Weather Updates" value={weatherFormState} onChange={handleChange} name="title" />
             <button>Search Local Weather Conditions</button>
         </form>
 
+
+
+
+
     );
-}
+
+    }
+
+    <Card>
+      <Card.Img src="https://picsum.photos/200/300" />
+      <Card.Body>
+        <Card.Title>Title Text</Card.Title>
+        <Card.Text>
+          Here's some fillllllller text
+        </Card.Text>
+        <div className="d-flex justify-content-between">
+          <Button className="ms-3">Add</Button>
+          <Button className="me-3">Remove</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  
+
+
+    
